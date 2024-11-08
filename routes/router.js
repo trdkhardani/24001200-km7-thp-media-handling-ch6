3c76668e0fcfae9ba333d65860b68171c5fab27d
Tridiktya Putra <tridiktya@gmail.com>
2024-11-08 21:29:15 +0700

    feat(api): add root endpoint to display available API endpoints

--- routes/router.js
+++ routes/router.js
@@ -10,9 +10,19 @@ const deleteImage = require('../controllers/delete_image.controller')
 const editImage = require('../controllers/edit_image.controller')
 
 app.get('/', (req, res, next) => {
-    res.json({
+    const baseUrl = `${req.protocol}://${req.get('host')}`
+    const endpointsList = {
+        add_image: `POST ${baseUrl}/add-image`,
+        get_all_images: `GET ${baseUrl}/get-all-images`,
+        get_single_image: `GET ${baseUrl}/get-image/{id}`,
+        delete_image: `DELETE ${baseUrl}/delete-image/{id}`,
+        update_image: `PATCH ${baseUrl}/edit-image/{id}`,
+    }
+
+    return res.json({
         status: 'success',
-        message: 'It works!'
+        message: 'It works!',
+        available_endpoints: endpointsList
     })
 })
 
